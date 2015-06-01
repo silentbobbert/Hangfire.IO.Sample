26f424cc4b639770ee073c91f6088f2558049b87
Ian Robertson <ian.danger.robertson+github@gmail.com>
2015-06-01 10:32:03 +0100

    Add a default user with correct roles/claims to use hangfire if there are no users present in DB

--- Hangfire.Sample.Repository/Migrations/Configuration.cs
+++ Hangfire.Sample.Repository/Migrations/Configuration.cs
@@ -1,7 +1,11 @@
+using System.Linq;
+using System.Security.Claims;
 using Hangfire.Sample.Repository.EF;
 using Hangfire.Sample.Repository.Identity;
 using System;
 using System.Data.Entity.Migrations;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Hangfire.Sample.Repository.Migrations
 {
@@ -22,6 +26,27 @@ namespace Hangfire.Sample.Repository.Migrations
                 new AppRole { Id = Guid.Parse("C7E132F8-CEFA-4EDE-9F0D-0C8D791EBDFB"), Name = "SuperUser" },
                 new AppRole { Id = Guid.Parse("B142996D-EAC5-431E-9331-3436E5AA92C9"), Name = "Basic" }
             );
+
+            if (!context.Users.Any())
+            {
+                var passwordHasher = new PasswordHasher();
+                var password = passwordHasher.HashPassword("Change.Me");
+
+                var defaultUser = new ApplicationUser
+                {
+                    Id = Guid.Parse("6A2D687D-4E9E-4675-A2B3-7286C4064A1C"),
+                    UserName = "hangfire@sample.co.uk",
+                    Email = "hangfire@sample.co.uk",
+                    LockoutEnabled = true,
+                    EmailConfirmed = true,
+                    PasswordHash = password,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                defaultUser.Roles.Add(new AppUserRole { RoleId = Guid.Parse("A843A55F-6B90-4F66-8666-B5155A5735D6"), UserId = Guid.Parse("6A2D687D-4E9E-4675-A2B3-7286C4064A1C") });
+                defaultUser.Claims.Add(new AppUserClaim { UserId = Guid.Parse("6A2D687D-4E9E-4675-A2B3-7286C4064A1C"), ClaimType = "hangfireAccess", ClaimValue = "true" });
+                context.Users.AddOrUpdate(u => u.Id, defaultUser);
+            }
         }
     }
 }
