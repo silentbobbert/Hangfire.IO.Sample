1d64c06f124b5022672bfb0005e86176b704bac9
silentbobbert <ian.danger.robertson+github@gmail.com>
2015-06-02 20:21:17 +0100

    Outputting a JSON data string via Signal R - could use with knockout model to update UI.

--- Hangfire.IO.Sample.BusinessLogic/Worker.cs
+++ Hangfire.IO.Sample.BusinessLogic/Worker.cs
@@ -1,13 +1,23 @@
-﻿using Hangfire.IO.Sample.BusinessLogic.Annotations;
+﻿using System.Linq;
+using Hangfire.IO.Sample.BusinessLogic.Annotations;
 using Hangfire.IO.Sample.BusinessLogic.Hubs;
 using log4net;
-using log4net.Config;
+using Newtonsoft.Json;
 using ParallelProcessing.BusinessLogic;
 using System;
 using System.Diagnostics;
 
 namespace Hangfire.IO.Sample.BusinessLogic
 {
+    public struct PrimeNumberSearchResult
+    {
+        public DateTime Queued { get; set; }
+        public DateTime Processed { get; set; }
+        public Int64 ProcessingTime { get; set; }
+        public int NumberOfPrimes { get; set; }
+        public UInt64 MaxPrime { get; set; }
+        public uint UpperLimit { get; set; }
+    }
     [UsedImplicitly]
     public class Worker : IWorker
     {
@@ -28,17 +38,30 @@ namespace Hangfire.IO.Sample.BusinessLogic
 
             s.Stop();
 
+            var result = new PrimeNumberSearchResult
+            {
+                Queued = queuedAtDateTime,
+                Processed = DateTime.Now,
+                ProcessingTime = s.ElapsedMilliseconds,
+                NumberOfPrimes = results.Count,
+                MaxPrime = results.Max(),
+                UpperLimit = upperLimit
+            };
+
             var message = string.Format("This Job was queued at {0} and then Processed at {1}. " +
                                         "The task took {2}ms. " +
                                         "The number of primes found was {3}. " +
-                                        "The upper limit was {4}", 
-                queuedAtDateTime, 
-                DateTime.Now,
-                s.ElapsedMilliseconds,
-                results.Count,
-                upperLimit
+                                        "The upper limit was {4}. The largest prime was  {5}",
+                result.Queued , 
+                result.Processed,
+                result.ProcessingTime,
+                result.NumberOfPrimes,
+                result.UpperLimit,
+                result.MaxPrime
             );
+
             NotificationHub.Instance.Notify(message);
+            NotificationHub.Instance.Notify(JsonConvert.SerializeObject(result));
             Debug.WriteLine(message);
             Log.Info(message);
         }
